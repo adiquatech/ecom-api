69df3911c4e329a87808bd069ee40e5f5e2a8730
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-11-16 14:06:48 +0100

    3rd test

--- models/product.js
+++ models/product.js
@@ -8,6 +8,7 @@ const productSchema = new mongoose.Schema(
     price: { type: Number, required: true, min: 0 },
     stock: { type: Number, required: true, min: 0 },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'category', required: true },
+    sku: { type: String, required: true, unique: true, trim: true },
     imageUrl: { type: String, default: '' },
     weight: { type: Number, min: 0 },
     dimensions: { type: String },
@@ -17,13 +18,27 @@ const productSchema = new mongoose.Schema(
     timestamps: true,
     toJSON: {
       transform: (doc, ret) => {
-        ret.id = ret._id.toString();   // _id → id
-        delete ret._id;                // remove _id
-        delete ret.__v;                // remove version
+        ret.id = ret._id.toString();   
+        delete ret._id;               
+        delete ret.__v;              
         return ret;
       }
     }
   }
 );
+
+// Auto-Generate sku Before Save
+productSchema.pre('save', async function (next) {
+  if (this.isNew && !this.sku) {
+    let sku;
+    const base = this.name.replace(/\s+/g, '').slice(0, 4).toUpperCase();
+    do {
+      const random = Math.floor(1000 + Math.random() * 9000);
+      sku = `${base}-${random}`;
+    } while (await this.constructor.findOne({ sku }));
+    this.sku = sku;
+  }
+  next();
+});
 
 export default mongoose.model('product', productSchema);
