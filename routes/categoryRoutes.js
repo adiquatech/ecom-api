63600805d9d15d30adb0ff47e5d2b4583f94bdc9
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-11-17 10:47:17 +0100

    ecom-api test

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,6 +4,23 @@ import { validateCategory } from '../middleware/validate.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Create a new category
+ *     tags: [Categories]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Category'
+ *     responses:
+ *       201:
+ *         description: Category created successfully
+ */
+
 router.route('/')
   .get(getCategories)
   .post(validateCategory, createCategory);
