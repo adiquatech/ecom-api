96480d030b836b87b4fcc5d2b8597dc947d000c6
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-11-23 12:45:58 +0100

    test4

--- swagger.js
+++ swagger.js
@@ -8,32 +8,22 @@ const options = {
     info: {
       title: 'E-commerce API - Week 3 & 4',
       version: '1.0.0',
-      description: 'Full CRUD API for 2 collections Products and Categories',
+      description: 'Full CRUD API with Google OAuth (Session-based)',
     },
     servers: [
       { url: 'https://ecom-api-lcxh.onrender.com/api' },
       { url: 'http://localhost:3000/api' },
     ],
-
     components: {
       securitySchemes: {
-        googleOAuth2: {
-          type: 'oauth2',
-          flows: {
-            authorizationCode: {
-              authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
-              tokenUrl: 'https://oauth2.googleapis.com/token',
-              scopes: {
-                openid: 'OpenID Connect',
-                email: 'View your email',
-                profile: 'View your profile',
-              },
-            },
-          },
+        cookieAuth: {
+          type: 'apiKey',
+          in: 'cookie',
+          name: 'connect.sid',
         },
       },
     },
-    security: [{ googleOAuth2: ['openid', 'email', 'profile'] }],
+    security: [{ cookieAuth: [] }],
   },
   apis: [
     './routes/productRoutes.js',
